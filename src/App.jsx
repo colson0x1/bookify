56fba32ff607acd5d3db19b508628f696f4b6e42
Colson <colsoncalifornication@gmail.com>
2023-06-27 14:00:26 +0545

    receive new title and style BookCreate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log('Need to add book with:', title);
+  };
+
   return (
     <div>
-      <h1>App</h1>
+      <BookCreate onCreate={createBook} />
     </div>
   );
 }
--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -1,9 +1,28 @@
-function BookCreate() {
+import { useState } from 'react';
+
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState('');
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle('');
+  };
+
   return (
-    <div>
-      <h1>BookCreate</h1>
+    <div className="book-create">
+    <h3>Add a Book</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input className="input" value={title} onChange={handleChange} />
+        <button className="button">Create</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default BookCreate;
