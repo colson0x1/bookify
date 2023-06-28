ba2cceb9028c066eca7ddb2d98eec39440c33823
Colson <colsoncalifornication@gmail.com>
2023-06-28 14:15:29 +0545

    use axios to fetch over api to create new record

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
@@ -16,10 +17,16 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title
+    })
+
+    console.log(response);
+
     const updatedBooks = [
       ...books,
-      { id: Math.round(Math.random() * 9999), title },
+      response.data
     ];
 
     setBooks(updatedBooks);
