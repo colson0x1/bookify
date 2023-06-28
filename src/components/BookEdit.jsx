53b62ee36826696f3c5c9db03c89ee7b10624648
Colson <colsoncalifornication@gmail.com>
2023-06-28 07:33:31 +0545

    implement state on form and add default value to the title

--- src/components/BookEdit.jsx
+++ src/components/BookEdit.jsx
@@ -1,8 +1,24 @@
-function BookEdit() {
+import { useState } from 'react';
+
+function BookEdit({ book }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('New title is', title);
+  };
+
   return (
-    <div>
-      <h1>BookEdit</h1>
-    </div>
+    <form onSubmit={handleSubmit} className="book-edit">
+      <label>Title</label>
+      <input className="input" value={title} onChange={handleChange} />
+      <button className="button is-primary">Save</button>
+    </form>
   );
 }
 
