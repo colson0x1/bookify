ef3f413be3155396520d1f7278cb74eb743dfdb3
Colson <colsoncalifornication@gmail.com>
2023-06-29 14:35:48 +0545

    refactor index, books and App

--- src/context/books.js
+++ src/context/books.js
@@ -1,14 +1,52 @@
 import { createContext, useState } from 'react';
+import axios from 'axios';
 
 const BooksContext = createContext();
 
-function Provider({ children }) { 
+function Provider({ children }) {
+  const [books, setBooks] = useState([]);
 
-  return (
-    <BooksContext.Provider value={{}}>
-      {children}
-    </BooksContext.Provider>
-  );
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+
+    setBooks(response.data);
+  };
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, ...response.data };
+      }
+      return book;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title,
+    });
+
+    console.log(response);
+
+    const updatedBooks = [...books, response.data];
+
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = async (id) => {
+    const response = axios.delete(`http://localhost:3001/books/${id}`);
+
+    const updatedBooks = books.filter((book) => book.id !== id);
+    setBooks(updatedBooks);
+  };
+
+  return <BooksContext.Provider value={{}}>{children}</BooksContext.Provider>;
 }
 
 export { Provider };
